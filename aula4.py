909936d7909a106cf06ab54032476542d51ac99b
Luiz Otávio <luizomf@gmail.com>
2020-01-21 00:11:20 -0300

    Greedy

---
+++ aula4.py
@@ -0,0 +1,12 @@
+# Meta caracteres: ^ $ ( )
+# * 0 ou n
+# + 1 ou n
+# ? 0 ou 1
+import re
+
+texto = '''
+<p>Frase 1</p> <p>Eita</p> <p>Qualquer frase</p> <div>1</div> 
+'''
+
+print(re.findall(r'<[dpiv]{1,3}>.+<\/[dpiv]{1,3}>', texto))
+print(re.findall(r'<[dpiv]{1,3}>.+?<\/[dpiv]{1,3}>', texto))
