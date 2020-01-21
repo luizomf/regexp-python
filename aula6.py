00f557f4106b196be3b373cdef6142db0f2e80f4
Luiz Otávio <luizomf@gmail.com>
2020-01-21 13:00:53 -0300

    ^ e $

---
+++ aula6.py
@@ -0,0 +1,10 @@
+# Meta caracteres:
+# ^ -> começa com
+# $ -> termina com
+# [^a-z] -> Negação
+
+import re
+
+cpf = '147.852.963-12'
+print(re.findall(r'^((?:[0-9]{3}\.){2}[0-9]{3}-[0-9]{2})$', cpf))
+print(re.findall(r'[^0-9a-zA-Z.-]+', cpf))
