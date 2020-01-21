29c141a14f35eb158f5aa96449f22811b97e9d80
Luiz Otávio <luizomf@gmail.com>
2020-01-21 18:04:22 -0300

    Lookaround

---
+++ aula9.py
@@ -0,0 +1,30 @@
+import re
+from pprint import pprint
+
+texto = '''
+ONLINE  192.168.0.1 GHIJK active
+OFFLINE  192.168.0.2 GHIJK inactive
+OFFLINE  192.168.0.3 GHIJK active
+ONLINE  192.168.0.4 GHIJK active
+ONLINE  192.168.0.5 GHIJK inactive
+OFFLINE  192.168.0.6 GHIJK active
+'''
+
+# Positive lookahead
+# pprint(re.findall(r'\w+\s+(\d+\.\d+\.\d+\.\d+)\s+\w+\s+(?=active)', texto))
+
+# Negative lookahead
+# pprint(re.findall(r'\w+\s+(\d+\.\d+\.\d+\.\d+)\s+\w+\s+(?!active)', texto))
+
+# Positive lookahead
+# pprint(re.findall(r'(?=.*[^in]active).+', texto))
+
+# Positive lookbehind
+# pprint(re.findall(r'\w+(?<=ONLINE)\s+(\d+\.\d+\.\d+\.\d+)\s+\w+\s+\w+', texto))
+# Negative lookbehind
+# pprint(re.findall(r'\w+(?<!ONLINE)\s+(\d+\.\d+\.\d+\.\d+)\s+\w+\s+\w+', texto))
+
+# Positive lookbehind
+# pprint(re.findall(r'\w+(?<=OFFLINE)\s+\d+\.\d+\.\d+\.\d+\s+\w+\s+\w+', texto))
+# Negative lookbehind
+# pprint(re.findall(r'\w+(?<!OFFLINE)\s+\d+\.\d+\.\d+\.\d+\s+\w+\s+\w+', texto))
